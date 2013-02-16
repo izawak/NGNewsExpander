63a69babdc31b46a905bb323039748cdd173f3c9
izawak <kai72@me.com>
2013-02-16 12:42:19 +0900

    first commit

---
+++ ExpandNGNews.js
@@ -0,0 +1,8 @@
+// Expand NationalGeographicNews Automatically
+
+var dd = document.getElementById('DispDefault1');
+var df = document.getElementById('DispFull1');
+if(df && dd){
+  dd.style.display="none";
+  df.style.display="block";
+}
